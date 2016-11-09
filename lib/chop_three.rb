d3171cb64e6f33b71964a7c3f86b8534f16be336
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-09 13:01:58 +0000

    Refactor ternary operator in chop_three to improve readibility

--- lib/chop_three.rb
+++ lib/chop_three.rb
@@ -2,11 +2,12 @@ class ChopThree
 
   def self.chop(search_term, sorted_array)
     return 0 if search_term == sorted_array[0]
-
     return -1 if sorted_array == []
 
     index_of_item_in_tail = self.chop(search_term, sorted_array[TAIL])
-    (index_of_item_in_tail == -1) ? -1 : (1 + index_of_item_in_tail)
+
+    return -1 if index_of_item_in_tail == -1
+    1 + index_of_item_in_tail
   end
 
   TAIL = (1..-1)
